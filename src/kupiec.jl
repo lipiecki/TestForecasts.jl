9141b147f52604e3e970ab90be29be849ebd3b4a
Arkadiusz Lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2026-01-21 19:29:21 +0100

    handle edge cases of empirical coverage

--- src/kupiec.jl
+++ src/kupiec.jl
@@ -4,10 +4,17 @@
 function kupiec(obs::AbstractVecOrMat{<:Real}, forecast::AbstractVecOrMat{<:Real}, level::AbstractFloat)
     @assert size(obs) == size(forecast)
     @assert size(obs) == size(forecast)
-    @assert level > 0 && level < 1
+    @assert level > 0 && !(level ≈ 0) 
+    @assert level < 1 && !(level ≈ 1)
     n = length(obs)
     nhits = sum(obs .< forecast)
-    test_statistic = (n-nhits)*log(1-nhits/n) + nhits*log(nhits/n) - (n-nhits)*log(1-level) - nhits*log(level)
+    if nhits == 0
+        test_statistic = -n*log(1-level)
+    elseif nhits == n
+        test_statistic = -nhits*log(level)
+    else
+        test_statistic = (n-nhits)*log(1-nhits/n) + nhits*log(nhits/n) - (n-nhits)*log(1-level) - nhits*log(level)
+    end
     test_statistic *= 2
     return 1-cdf(Chisq(1), test_statistic)
 end
