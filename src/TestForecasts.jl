4e4c729b67de8a2194778ec8267bb3732df3d403
lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2026-09-16 21:40:42 +0200

    update exports

--- src/TestForecasts.jl
+++ src/TestForecasts.jl
@@ -7,14 +7,20 @@ include("losses.jl")
 include("dieboldmariano.jl")
 include("giacominiwhite.jl")
 include("kupiec.jl")
+#TODO: include("mcs.jl") 
 
 export
+    # losses
     pnorm,
     squared,
     absolute,
     pinball,
+    crps,
+    aps,
     
+    # tests
     dieboldmariano,
     giacominiwhite,
     kupiec
+    #TODO: mcs
 end
