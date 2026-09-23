6207ed35e1966ea62e20fcfbf80824b13cd60daf
lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2026-09-23 14:26:32 +0200

    generalize losses to work on 0-dimensional views

--- src/losses.jl
+++ src/losses.jl
@@ -1,14 +1,34 @@
+const AbstractZeroDimOrVec{T} = Union{AbstractArray{T,0}, AbstractVector{T}}
 
-function pnorm(y::AbstractVector{<:Real}, x::AbstractVector{<:Real}, p::AbstractFloat=2)
+function pnorm(y::AbstractArray{<:Real,0}, x::AbstractArray{<:Real,0}, ::AbstractFloat=2.0)
+    return abs(y[begin] - x[begin])
+end
+
+function pnorm(y::AbstractVector{<:Real}, x::AbstractVector{<:Real}, p::AbstractFloat=2.0)
     return norm(y .- x, p)
+end
+
+function squared(y::AbstractArray{<:Real,0}, x::AbstractArray{<:Real,0})
+    return abs2(y[begin] - x[begin])
 end
 
 function squared(y::AbstractVector{<:Real}, x::AbstractVector{<:Real})
     return sum(abs2, y .- x) / length(y)
 end
 
+function absolute(y::AbstractArray{<:Real,0}, x::AbstractArray{<:Real,0})
+    return abs(y[begin] - x[begin])
+end
+
 function absolute(y::AbstractVector{<:Real}, x::AbstractVector{<:Real})
     return sum(abs, y .- x) / length(y)
+end
+
+function pinball(y::AbstractArray{<:Real, 0}, x::AbstractArray{<:Real, 0}, level::AbstractFloat)
+    loss = 0.0
+    diff = y[begin] - x[begin]
+    loss += (level - (diff < 0 ? 1.0 : 0.0)) * diff
+    return loss / length(y)
 end
 
 function pinball(y::AbstractVector{<:Real}, x::AbstractVector{<:Real}, level::AbstractFloat)
@@ -20,7 +40,19 @@ function pinball(y::AbstractVector{<:Real}, x::AbstractVector{<:Real}, level::Ab
     return loss / length(y)
 end
 
+function crps(y::AbstractArray{<:Real, 0}, x::AbstractVector{<:Real}, nlevels::Int=99)
+    @assert length(x) = nlevels
+    loss = 0.0
+    for j in 1:nlevels
+        tau = j/(nlevels+1)
+        diff = y[begin] - x[i, j]
+        loss += (tau - (diff < 0 ? 1.0 : 0.0)) * diff
+    end
+    return 2 * loss / nlevels
+end
+
 function crps(y::AbstractVector{<:Real}, x::AbstractMatrix{<:Real}, nlevels::Int=99)
+    @assert size(x, 2) == nlevels
     loss = 0.0
     for i in eachindex(y)
         for j in 1:nlevels
@@ -32,13 +64,23 @@ function crps(y::AbstractVector{<:Real}, x::AbstractMatrix{<:Real}, nlevels::Int
     return 2 * loss / length(y) / nlevels
 end
 
+function aps(y::AbstractArray{<:Real, 0}, x::AbstractVector{<:Real}, levels::AbstractVector{<:Real})
+    @assert length(x) = length(levels)
+    loss = 0.0
+    for j in eachindex(levels)
+        diff = y[begin] - x[j]
+        loss += (levels[j] - (diff < 0 ? 1.0 : 0.0)) * diff
+    end
+    return loss / length(levels)
+end
+
 function aps(y::AbstractVector{<:Real}, x::AbstractMatrix{<:Real}, levels::AbstractVector{<:Real})
+    @assert size(x, 2) == length(levels)
     loss = 0.0
     for i in eachindex(y)
-        for j in 1:length(levels)
-            tau = levels[j]
+        for j in eachindex(levels)
             diff = y[i] - x[i, j]
-            loss += (tau - (diff < 0 ? 1.0 : 0.0)) * diff
+            loss += (levels[j] - (diff < 0 ? 1.0 : 0.0)) * diff
         end
     end
     return loss / length(y) / length(levels)
